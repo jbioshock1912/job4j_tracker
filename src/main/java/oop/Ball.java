f247753d9f1de1df5e2fbf38fb80f2e260f60f69
Yuliya.Sabirova <Yuliya.Sabirova@bercut.com>
2023-11-01 21:27:34 +0300

    Взаимодействие объектов [#166087]

---
+++ src/main/java/oop/Ball.java
@@ -0,0 +1,9 @@
+package oop;
+
+public class Ball {
+
+    public void tryRun(boolean condition){
+        String message = condition ? "колобок съеден" : "колобок сбежал";
+        System.out.println(message);
+    }
+}
---
+++ src/main/java/oop/Wolf.java
@@ -0,0 +1,11 @@
+package oop;
+
+public class Wolf {
+
+    public void eat(Girl girl) {
+    }
+
+    public void tryEat(Ball ball){
+        ball.tryRun(false);
+    }
+}
