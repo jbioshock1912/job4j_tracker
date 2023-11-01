3237686323fad4d966b30a1bcdd457719c29b163
Yuliya.Sabirova <Yuliya.Sabirova@bercut.com>
2023-11-01 19:33:07 +0300

    Создание объекта [#165452]

---
+++ src/main/java/oop/Dog.java
@@ -0,0 +1,9 @@
+package oop;
+
+public class Dog {
+    public static void main(String[] args) {
+        Dog jack = new Dog();
+        Dog volt = new Dog();
+        Dog leo = new Dog();
+    }
+}
