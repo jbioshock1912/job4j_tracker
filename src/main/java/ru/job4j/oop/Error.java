13d0a68176b49bc56d2a3d9b0ca086544585a1ce
Yuliya.Sabirova <Yuliya.Sabirova@bercut.com>
2023-11-02 09:17:11 +0300

    Конструктор [#173889]

---
+++ src/main/java/ru/job4j/oop/Error.java
@@ -0,0 +1,32 @@
+package ru.job4j.oop;
+
+public class Error {
+
+    boolean active;
+    int status;
+    String message;
+
+    public Error() {
+    }
+
+    public Error(boolean active, int status, String message) {
+        this.active = active;
+        this.status = status;
+        this.message = message;
+    }
+
+    public void printInfo() {
+        System.out.println("Активность ошибки " + active);
+        System.out.println("Номер статуса " + status);
+        System.out.println("Сообщение: " + message);
+    }
+
+    public static void main(String[] args) {
+        Error errorEmpty = new Error();
+        errorEmpty.printInfo();
+        Error graphicError = new Error(true, 2, "Графическая ошибка");
+        graphicError.printInfo();
+        Error publicationError = new Error(false, 0, "Ошибка публикации");
+        publicationError.printInfo();
+    }
+}
