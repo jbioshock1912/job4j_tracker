da9d61292ba5ec96b363ebc166496333f44b8b61
Yuliya.Sabirova <Yuliya.Sabirova@bercut.com>
2023-11-06 16:12:40 +0300

    Tracker - хранилище [#396]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -1,5 +1,7 @@
 package ru.job4j.tracker;
 
+import java.util.Arrays;
+
 public class Tracker {
     private final Item[] items = new Item[100];
     private int ids = 1;
@@ -21,5 +23,31 @@ public class Tracker {
             }
         }
         return rsl;
+    }
+
+    public Item[] findAll() {
+        Item[] result = new Item[items.length];
+        int size = 0;
+        for (Item itemForCheck : items) {
+            if (itemForCheck != null) {
+                result[size] = itemForCheck;
+                size++;
+            }
+        }
+        result = Arrays.copyOf(result, size);
+        return result;
+    }
+
+    public Item[] findByName(String key) {
+        Item[] result = new Item[items.length];
+        int size = 0;
+        for (Item itemForCheck : items) {
+            if (itemForCheck != null && key.equals(itemForCheck.getName())) {
+                result[size] = itemForCheck;
+                size++;
+            }
+        }
+        result = Arrays.copyOf(result, size);
+        return result;
     }
 }
