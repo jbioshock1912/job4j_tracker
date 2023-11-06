a665f8db6c05c7dfe811ecca430b11a364ce741e
Yuliya.Sabirova <Yuliya.Sabirova@bercut.com>
2023-11-06 15:26:05 +0300

    refactor измнеила цикл

--- src/main/java/ru/job4j/pojo/ShopDrop.java
+++ src/main/java/ru/job4j/pojo/ShopDrop.java
@@ -2,14 +2,10 @@ package ru.job4j.pojo;
 
 public class ShopDrop {
     public static Product[] delete(Product[] products, int index) {
-        if (products.length == 2 && index == 1) {
-            products[1] = null;
-        }
         for (int i = index; i < products.length - 1; i++) {
-            products[i] = null;
             products[i] = products[i + 1];
-            products[i + 1] = null;
         }
+        products[products.length - 1] = null;
         return products;
     }
 }
